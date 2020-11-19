dbaa0b9aa60ee6f7cd1e8ff57b949d931e57bd20
wangshuaijing <wangshuaijing@58.com>
2020-11-19 20:00:14 +0800

    解决启动时中文乱码问题

--- src/main/java/org/apache/tomcat/util/res/StringManager.java
+++ src/main/java/org/apache/tomcat/util/res/StringManager.java
@@ -16,6 +16,7 @@
  */
 package org.apache.tomcat.util.res;
 
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -131,6 +132,7 @@ public class StringManager {
             // Avoid NPE if bundle is null and treat it like an MRE
             if (bundle != null) {
                 str = bundle.getString(key);
+                str = new String(str.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
             }
         } catch (MissingResourceException mre) {
             //bad: shouldn't mask an exception the following way:
